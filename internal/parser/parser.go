ea91b66abadace91e177ac0f68ee2b8666585fec
M <m1@mishe1.ru>
2026-03-28 17:02:41 +0300

    Добавлено чтение и парсинг JSON-файлов - step3

---
+++ internal/parser/parser.go
@@ -0,0 +1,40 @@
+package parser
+
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func Parse(path string) (map[string]any, error) {
+	// 1. проверить расширение json
+	ext := filepath.Ext(path)
+	if strings.ToLower(ext) != ".json" {
+		return nil, errors.New("file isn't json")
+	}
+
+	// 2. открыть файл
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	// 3. прочитать содержимое
+	filedata, err := io.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
+
+	// 4. распарсить в json
+	var jsondata map[string]any // ???: а если массив в корне?
+	err = json.Unmarshal(filedata, &jsondata)
+	if err != nil {
+		return nil, err
+	}
+
+	return jsondata, nil
+}
---
+++ internal/parser/parser_test.go
@@ -0,0 +1,59 @@
+package parser
+
+import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+	"reflect"
+	"testing"
+)
+
+func Test_ParserUnmarshal_OK(t *testing.T) {
+	data := `{
+  		"host": "hexlet.io",
+  		"timeout": 50,
+  		"proxy": "123.234.53.22",
+  		"follow": false
+	}`
+
+	var want map[string]any
+	err := json.Unmarshal([]byte(data), &want)
+	if err != nil {
+		t.Fatal("failed prepare test")
+	}
+
+	dir := t.TempDir()
+	path := filepath.Join(dir, "test.json")
+
+	filedata := []byte(data)
+
+	err = os.WriteFile(path, filedata, 0644)
+	if err != nil {
+		t.Fatalf("write file: %v", err)
+	}
+
+	got, err := Parse(path)
+	if err != nil {
+		t.Fatalf("parse data: %v", err)
+	}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Fatalf("want: %#v, got: %#v", want, got)
+	}
+}
+
+func Test_ParserNotJSONFile(t *testing.T) {
+	dir := t.TempDir()
+	path := filepath.Join(dir, "test.txt")
+
+	filedata := []byte("")
+	err := os.WriteFile(path, filedata, 0644)
+	if err != nil {
+		t.Fatalf("write file: %v", err)
+	}
+
+	_, err = Parse(path)
+	if err == nil {
+		t.Fatalf("want err, got nil")
+	}
+}
