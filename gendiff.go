65fed7dbf15afd1c89eaeca352a0acc4040db8f5
M <m1@mishe1.ru>
2026-04-01 12:50:05 +0300

    refactor: Рефакторинг GenDiff

--- gendiff.go
+++ gendiff.go
@@ -8,66 +8,62 @@ import (
 	"strings"
 )
 
-func GenDiff(filePathLeft, filePathRight, format string) (string, error) {
+func GenDiff(leftPath, rightPath, format string) (string, error) {
 	_ = format // NOTE: пока не используем
 
-	// распарсить файлы
-	dataLeft, dataRight, err := parseFiles(filePathLeft, filePathRight)
+	leftData, rightData, err := parseFiles(leftPath, rightPath)
 	if err != nil {
 		return "", err
 	}
 
-	// собрать все ключи из обоих объектов
-	// отсортировать ключи
-	keysSorted := sortedKeys(dataLeft, dataRight)
+	keysSorted := sortedKeys(leftData, rightData)
+	diff := buildDiff(leftData, rightData, keysSorted)
 
-	// пройтись по ключам и собрать строки в diff (пока массив, но в итоге нужно будет дерево)
-	diff := buildDiff(dataLeft, dataRight, keysSorted)
-
-	// склеить все в итоговую строку
+	// склеить diff в итоговую строку
 	r := fmt.Sprintf("{\n%s\n}", strings.Join(diff, "\n"))
 
 	return r, nil
 }
 
-func parseFiles(filePath1, filePath2 string) (map[string]any, map[string]any, error) {
-	fileData1, err := parser.Parse(filePath1)
+func parseFiles(path1, path2 string) (map[string]any, map[string]any, error) {
+	data1, err := parser.Parse(path1)
 	if err != nil {
 		return nil, nil, err
 	}
 
-	fileData2, err := parser.Parse(filePath2)
+	data2, err := parser.Parse(path2)
 	if err != nil {
 		return nil, nil, err
 	}
 
-	return fileData1, fileData2, nil
+	return data1, data2, nil
 }
 
 func sortedKeys(data1, data2 map[string]any) []string {
-	keysSet := make(map[string]struct{})
+	set := make(map[string]struct{})
 	for k := range data1 {
-		keysSet[k] = struct{}{}
+		set[k] = struct{}{}
 	}
 	for k := range data2 {
-		keysSet[k] = struct{}{}
+		set[k] = struct{}{}
 	}
 
-	keys := make([]string, 0, len(keysSet))
-	for k := range keysSet {
+	keys := make([]string, 0, len(set))
+	for k := range set {
 		keys = append(keys, k)
 	}
+
 	sort.Strings(keys)
 
 	return keys
 }
 
-func buildDiff(data1, data2 map[string]any, keys []string) []string {
-	diff := make([]string, 0)
+func buildDiff(leftData, rightData map[string]any, keys []string) []string {
+	diff := make([]string, 0, len(keys))
 
 	for _, key := range keys {
-		v1, ok1 := data1[key]
-		v2, ok2 := data2[key]
+		v1, ok1 := leftData[key]
+		v2, ok2 := rightData[key]
 
 		switch {
 		case !ok1:
